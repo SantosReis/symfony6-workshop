2eb4ab733a0615d44f77f20b1dc2d8a085b83f4b
Marcus Reis <mvsantosreis@gmail.com>
2024-04-14 20:20:27 +0100

    redirectToRoute

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,17 +13,31 @@ class ProductController extends AbstractController
     #[Route('/', name: 'all', methods: ['GET', 'HEAD'])]
     public function index(): Response
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
-    }
-
-    #[Route('/{slug}', name: 'name')]
-    public function show(Product $product): Response
-    {
-        return new Response('Displaying product: ' . $product->getName());
+        return $this->redirectToRoute('products_inventory');
         // return $this->render('product/index.html.twig', [
         //     'controller_name' => 'ProductController',
         // ]);
     }
+
+    // #[Route('/{slug}', name: 'name')]
+    // public function show(Product $product): Response
+    // {
+    //     return new Response('Displaying product: ' . $product->getName());
+    //     // return $this->render('product/index.html.twig', [
+    //     //     'controller_name' => 'ProductController',
+    //     // ]);
+    // }
+
+    #[Route('/inventory', name: 'inventory')]
+    public function inventory(): Response
+    {
+        return new Response('This is product inventory page');
+    }
+
+    // #[Route('/redirect', name: 'redirect')]
+    // public function test_redirect(): Response
+    // {
+    //     return $this->redirectToRoute('products_inventory');
+    // }
+
 }
