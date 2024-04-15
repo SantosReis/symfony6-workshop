1a6438ce418dfa73e9270f2db5d7390f901466c1
Marcus Reis <mvsantosreis@gmail.com>
2024-04-15 17:28:07 +0100

    Symfony Mailer - Send Email (with attached file) as Twig Template

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,14 +13,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class EmailController extends AbstractController
 {
     #[Route('/email', name: 'app_email')]
-    public function index(MailerInterface $mailer): Response
+    public function index(MailerInterface $mailer, $appEmail, $publicDir)
     {
 
-        $email = (new Email())
-            ->from('sales@app.com')
-            ->to('email@example.com')
+        $email = (new TemplatedEmail())
+            ->from($appEmail)
+            ->to(new Address('email@example.com', 'Gary'))
             ->subject('Your order has been placed')
-            ->html('<p>Thank you, your order has been placed</p>');
+            ->htmlTemplate('email/order-confirmation.html.twig')
+            ->attachFromPath($publicDir . '/pdf/test.pdf')
+            ->context([
+                'delivery_date' => date_create('+3 days'),
+                'order_number' => rand(5, 50000),
+            ]);
 
         $mailer->send($email);
 
