d5e10a8db1454e5906e9b0e72c50657cee62c9f0
Marcus Reis <mvsantosreis@gmail.com>
2024-06-29 09:50:45 +0100

    User Registration

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
